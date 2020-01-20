b08499fb33c21cd1aeb97c3f5f03c60a0e985a9c
Trie0037 <trie0037@gmail.com>
2020-01-20 13:36:34 -0600

    scanclose

--- JavaCalculator/src/calculator/Calculator.java
+++ JavaCalculator/src/calculator/Calculator.java
@@ -3,51 +3,49 @@ package calculator;
 import java.util.Scanner;
 
 public class Calculator {
-
+	
 	static int answer;
 
 	public static void main(String[] args) {
 		int firstNum;
 		int secondNum;
 		int operator;
-
 		Scanner scanner = new Scanner(System.in);
-
-		System.out.print("Enter the first number ");
+		
+		System.out.print("Enter the first number");
 		firstNum = scanner.nextInt();
-		System.out.print("Enter the second number ");
+		System.out.print("Enter the second number");
 		secondNum = scanner.nextInt();
 		System.out.print("Select operator: 1(+), 2(-), 3(*), 4(/), 5(%)");
 		operator = scanner.nextInt();
-
+		
 		int solution = calculate(firstNum, secondNum, operator);
-
+		
 		System.out.println("Your answer is " + solution);
-
-		scanner.close();
+		
 
 	}
 
 	private static int calculate(int firstNum, int secondNum, int operator) {
-		switch (operator) {
-		case 1:
+		switch(operator) {
+		case 1: 
 			answer = firstNum + secondNum;
-			break;
-		case 2:
+		break;
+		case 2: 
 			answer = firstNum - secondNum;
-			break;
-		case 3:
+		break;
+		case 3: 
 			answer = firstNum * secondNum;
-			break;
-		case 4:
+		break;
+		case 4: 
 			answer = firstNum / secondNum;
-			break;
-		case 5:
+		break;
+		case 5: 
 			answer = firstNum % secondNum;
-
+		
 		}
 		return answer;
-
+		
 	}
 
 }
